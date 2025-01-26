c35b3e3621f38ae3efd4bd6874fee90e6a5d4dee
Napenjoyoor <napenjoyoor@proton.me>
2025-01-26 22:38:56 +1100

    chore: cleanup imports to be explicit

--- src/MintableBurnableToken.sol
+++ src/MintableBurnableToken.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.20;
 
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable2Step, Ownable} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
 
 /**
  * @title The STRAT Token
