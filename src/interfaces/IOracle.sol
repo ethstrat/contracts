eb9838f195b52a9a2ecd4cb99a6e3dff50c25538
Napenjoyoor <napenjoyoor@proton.me>
2025-02-01 21:44:31 +1100

    fix: updated comments on IOracle interface

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -1,8 +1,13 @@
 pragma solidity ^0.8.0;
 
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+/// @title IOracle
+/// @dev Oracle interface used throughout the protocol.
 interface IOracle {
-    /**
-     * @notice Get the price of an asset, scaled by 1e8
-     */
+    /// @notice the price of 1 base token quoted in 1 of quote token
+    /// @dev It corresponds to the price of 10**(base token decimals) quoted in
+    /// 10**(quote token decimals)
     function price() external view returns (uint256);
 }
