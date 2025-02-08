25d7820e2f9f44df133b8c7e09de2e9533d23865
Napenjoyoor <napenjoyoor@proton.me>
2025-02-08 22:33:28 +1100

    feat: wip, debt redemption implementation

---
+++ src/StratOptionRedeemUSDNotional.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.8.20;
+
+import {StratOption} from "./StratOption.sol";
+import {IERC20, IERC20MintableBurnable} from "./interfaces/IERC20.sol";
+import {IOracle} from "./interfaces/IOracle.sol";
+
+interface Treasury {
+    function withdraw(uint256 amount) external;
+    function total() external view returns (uint256);
+}
+
+contract StratOptionRedeemUSDNotional {
+    IERC20MintableBurnable public immutable cdtToken;
+    IERC20 public immutable stratToken;
+    Treasury public immutable treasury;
+    IOracle public immutable ethUsdOracle;
+    StratOption public immutable stratOption;
+
+    error NotOwnerOrApproved(address account, uint256 tokenId);
+    error TimelockActive(address account, uint256 tokenId);
+    error OptionUnexpired(address account, uint256 tokenId);
+
+    // TODO(nap): Events
+
+    /**
+     * @param _cdtToken The CDT token
+     * @param _stratOption The STRAT option
+     * @param _treasury STRAT treasury
+     * @param _ethUsdOracle The ETH/USD oracle
+     * @param _stratOption The STRAT option
+     */
+    constructor(
+        address _cdtToken,
+        address _stratToken,
+        address _treasury,
+        address _ethUsdOracle,
+        address _stratOption
+    ) {
+        cdtToken = IERC20MintableBurnable(_cdtToken);
+        stratToken = IERC20(_stratToken);
+        treasury = Treasury(_treasury);
+        ethUsdOracle = IOracle(_ethUsdOracle);
+        stratOption = StratOption(_stratOption);
+    }
+
+    function redeemCdtForUsdNotional(uint256 tokenId) external {
+        if (stratOption.timelock(tokenId) < block.timestamp) revert TimelockActive(msg.sender, tokenId);
+        if (stratOption.expiry(tokenId) < block.timestamp) revert OptionUnexpired(msg.sender, tokenId);
+
+        address optionOwner = stratOption.ownerOf(tokenId);
+
+        if (optionOwner != msg.sender && stratOption.getApproved(tokenId) != msg.sender) {
+            revert NotOwnerOrApproved(msg.sender, tokenId);
+        }
+
+        uint256 notionalUSDAmount = stratOption.notionalUSDAmount(tokenId);
+        cdtToken.burnFrom(msg.sender, notionalUSDAmount);
+        stratOption.burn(tokenId);
+
+        uint256 ethPriceUSD = ethUsdOracle.price();
+        uint256 treasuryInUSD = treasury.total() * ethPriceUSD;
+        if (treasury.total() * ethPriceUSD < cdtToken.totalSupply()) {
+            // TODO(nap): handle (or at least think about) what happens with really small (ie, 1 CDT or less) bonds
+            treasury.withdraw(notionalUSDAmount / ethPriceUSD);
+        } else {
+            treasury.withdraw(notionalUSDAmount * treasuryInUSD / cdtToken.totalSupply());
+        }
+    }
+}
