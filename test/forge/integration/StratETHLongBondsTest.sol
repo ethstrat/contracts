cd959954f100c91a614ba507e7c0bcf9312e040f
Jem <0x0xjem@gmail.com>
2025-04-03 18:35:47 +0300

    Update long bonds tests

--- test/forge/integration/StratETHLongBondsTest.sol
+++ test/forge/integration/StratETHLongBondsTest.sol
@@ -6,6 +6,7 @@ import "../../../src/StratETHLongBonds.sol";
 import "../../../src/CdtToken.sol";
 import "../../../src/StratToken.sol";
 import "../../../src/StratOption.sol";
+import {IStratOptionMinter} from "../../../src/interfaces/IStratOptionMinter.sol";
 
 contract MockOracle {
     uint256 private _price;
@@ -92,7 +93,7 @@ contract StratETHLongBondsTest is Test {
         stratEthOracle.setQuoteTokenDecimals(8);
         stratEthOracle.setPrice(1e8); // 1 STRAT = 1 ETH
 
-        vm.expectRevert("StratEthOracle must have 18 decimals");
+        vm.expectRevert(abi.encodeWithSelector(StratETHLongBonds.InvalidParams.selector, "stratEthOracle"));
         new StratETHLongBonds(
             address(cdtToken),
             address(stratToken),
@@ -183,44 +184,44 @@ contract StratETHLongBondsTest is Test {
 
     function test_bond_noETH() public {
         vm.prank(user);
-        vm.expectRevert("No ETH sent");
+        vm.expectRevert(abi.encodeWithSelector(StratETHLongBonds.InvalidParams.selector, "msg.value"));
         bonds.bond(user);
     }
 
     function test_bond() public {
         uint256 ethAmount = 1 ether;
 
+        // Get the strike price
+        uint256 ethUsdValue = ethAmount * ethUsdOracle.price() / 1e8; // ETH -> USD conversion
+        uint256 expectedStrikePrice = bonds.strikePrice(ethUsdValue);
+
         vm.deal(user, ethAmount); // Give ETH to user
         vm.prank(user);
         // Run bond function
-        bonds.bond{value: ethAmount}(user);
+        uint256 tokenId = bonds.bond{value: ethAmount}(user);
 
         // Check treasury receives money
         assertEq(treasuryManager.balance, ethAmount, "Treasury did not receive the correct ETH amount");
 
         // Verify the CDT balance of the user
-        uint256 expectedCdUSDAmount = (ethAmount * ethUsdOracle.price()) / 1e8; // ETH -> USD conversion
-        assertEq(cdtToken.balanceOf(user), expectedCdUSDAmount, "User CDT balance incorrect");
+        assertEq(cdtToken.balanceOf(user), ethUsdValue, "User CDT balance incorrect");
 
-        // Verify the minted StratOption attributes
-        uint256 tokenId = 1; // Assuming this is the first minted option
-        uint256 expectedStrikeAmount = expectedCdUSDAmount;
+        // Balance = STRAT output when exercised
+        assertEq(stratOption.balanceOf(user, tokenId), 999500249875062468, "Incorrect balance");
 
-        // NFT Option properties
-        assertEq(stratOption.ownerOf(tokenId), user, "Incorrect owner");
-        assertEq(stratOption.strikeAmount(tokenId), expectedStrikeAmount, "Incorrect strike amount");
-        assertEq(
-            stratOption.notionalUnderlyingAmount(tokenId), 999500249875062468, "Incorrect notional underlying amount"
-        );
-        assertLt(
-            stratOption.notionalUnderlyingAmount(tokenId),
-            1e18, // 1 STRAT = 1 ETH
-            "Should be less than current spot price for strat"
-        );
-        assertEq(stratOption.notionalUSDAmount(tokenId), expectedCdUSDAmount, "Incorrect notional USD amount");
-        assertEq(stratOption.expiry(tokenId), block.timestamp + (4.2 * 365 days), "Incorrect expiry");
-        assertEq(stratOption.timelock(tokenId), block.timestamp + 69 minutes, "Incorrect timelock");
-        assertEq(stratOption.ownerOf(tokenId), user, "Incorrect owner");
+        IStratOptionMinter.Option memory option = stratOption.getOption(tokenId);
+
+        // Strike price
+        assertEq(option.strikePrice, expectedStrikePrice, "Incorrect strike price");
+
+        // Redemption price
+        assertEq(option.redemptionPrice, expectedStrikePrice, "Incorrect redemption price");
+
+        // Expiry
+        assertEq(option.expiry, block.timestamp + (4.2 * 365 days), "Incorrect expiry");
+
+        // Timelock
+        assertEq(option.timelock, block.timestamp + 69 minutes, "Incorrect timelock");
     }
 
     function test_bond_ethUsdOracleDecimals18() public {
@@ -246,65 +247,88 @@ contract StratETHLongBondsTest is Test {
 
         uint256 ethAmount = 1 ether;
 
+        // Get the strike price
+        uint256 ethUsdValue = ethAmount * 3000e18 / 1e18; // ETH -> USD conversion
+        uint256 expectedStrikePrice = bonds.strikePrice(ethUsdValue);
+
         vm.deal(user, ethAmount); // Give ETH to user
         vm.prank(user);
         // Run bond function
-        bonds.bond{value: ethAmount}(user);
+        uint256 tokenId = bonds.bond{value: ethAmount}(user);
 
         // Check treasury receives money
         assertEq(treasuryManager.balance, ethAmount, "Treasury did not receive the correct ETH amount");
 
         // Verify the CDT balance of the user
-        uint256 expectedCdUSDAmount = (ethAmount * 3000e18) / 1e18; // ETH -> USD conversion
-        assertEq(cdtToken.balanceOf(user), expectedCdUSDAmount, "User CDT balance incorrect");
+        assertEq(cdtToken.balanceOf(user), ethUsdValue, "User CDT balance incorrect");
 
-        // Verify the minted StratOption attributes
-        uint256 tokenId = 1; // Assuming this is the first minted option
-        uint256 expectedStrikeAmount = expectedCdUSDAmount;
+        // Balance = STRAT output when exercised
+        uint256 balance = stratOption.balanceOf(user, tokenId);
+        assertEq(balance, 999500249875062468, "Incorrect balance");
 
-        // NFT Option properties
-        assertEq(stratOption.ownerOf(tokenId), user, "Incorrect owner");
-        assertEq(stratOption.strikeAmount(tokenId), expectedStrikeAmount, "Incorrect strike amount");
-        assertEq(
-            stratOption.notionalUnderlyingAmount(tokenId), 999500249875062468, "Incorrect notional underlying amount"
-        );
-        assertLt(
-            stratOption.notionalUnderlyingAmount(tokenId),
-            1e18, // 1 STRAT = 1 ETH
-            "Should be less than current spot price for strat"
-        );
-        assertEq(stratOption.notionalUSDAmount(tokenId), expectedCdUSDAmount, "Incorrect notional USD amount");
-        assertEq(stratOption.expiry(tokenId), block.timestamp + (4.2 * 365 days), "Incorrect expiry");
-        assertEq(stratOption.timelock(tokenId), block.timestamp + 69 minutes, "Incorrect timelock");
-        assertEq(stratOption.ownerOf(tokenId), user, "Incorrect owner");
+        IStratOptionMinter.Option memory option = stratOption.getOption(tokenId);
+
+        // Strike price
+        assertEq(option.strikePrice, expectedStrikePrice, "Incorrect strike price");
+
+        // Balance * strike price = USD value
+        assertEq(balance * option.strikePrice / 1e18, ethUsdValue, "Incorrect strike amount");
+
+        // Redemption price
+        assertEq(option.redemptionPrice, expectedStrikePrice, "Incorrect redemption price");
+
+        // Balance * redemption price = USD value
+        assertEq(balance * option.redemptionPrice / 1e18, ethUsdValue, "Incorrect redemption amount");
+
+        // Expiry
+        assertEq(option.expiry, block.timestamp + (4.2 * 365 days), "Incorrect expiry");
+
+        // Timelock
+        assertEq(option.timelock, block.timestamp + 69 minutes, "Incorrect timelock");
     }
 
     function testBondDataInvariants() public {
+        uint256 previousTokenId;
+
         for (uint256 i = 0; i < 10; i++) {
             // Bond 1 ETH
             vm.deal(user, 1 ether);
             vm.prank(user);
-            bonds.bond{value: 1 ether}(user);
+            uint256 tokenId = bonds.bond{value: 1 ether}(user);
 
             if (i == 0) {
+                previousTokenId = tokenId;
                 continue;
             }
 
-            assertGt(
-                stratOption.notionalUnderlyingAmount(i),
-                stratOption.notionalUnderlyingAmount(i + 1),
-                "Each subsequent bond should have less notional than the previous"
+            IStratOptionMinter.Option memory currentOption = stratOption.getOption(tokenId);
+            IStratOptionMinter.Option memory previousOption = stratOption.getOption(previousTokenId);
+
+            // Strike price increases
+            assertGt(currentOption.strikePrice, previousOption.strikePrice, "Strike price should increase");
+
+            // STRAT output should decrease as the strike price increases
+            uint256 currentBalance = stratOption.balanceOf(user, tokenId);
+            uint256 previousBalance = stratOption.balanceOf(user, previousTokenId);
+            assertLt(
+                currentBalance, previousBalance, "Each subsequent bond should have less STRAT output than the previous"
             );
+
+            // Strike amount is the same
             assertEq(
-                stratOption.strikeAmount(i),
-                stratOption.strikeAmount(i + 1),
-                "Strike should be the same, as we are bonding the same amount of ETH each time (and the oracle isn't changing)"
+                currentOption.strikePrice * currentBalance / 1e18,
+                previousOption.strikePrice * previousBalance / 1e18,
+                "Strike amount should be the same"
             );
+
+            // Redemption amount is the same
             assertEq(
-                stratOption.notionalUSDAmount(i),
-                stratOption.strikeAmount(i + 1),
-                "Debt should be the same, as we are bonding the same amount of ETH each time (and the oracle isn't changing)"
+                currentOption.redemptionPrice * currentBalance / 1e18,
+                previousOption.redemptionPrice * previousBalance / 1e18,
+                "Redemption amount should be the same"
             );
+
+            previousTokenId = tokenId;
         }
     }
 
@@ -312,20 +336,19 @@ contract StratETHLongBondsTest is Test {
         // Bond 1 ETH
         vm.deal(user, 1 ether);
         vm.prank(user);
-        bonds.bond{value: 1 ether}(user);
+        uint256 tokenIdOne = bonds.bond{value: 1 ether}(user);
 
         // Bond 1 more ETH, after eth price goes up to $4000
         ethUsdOracle.setPrice(4000e8);
         vm.deal(user, 1 ether);
         vm.prank(user);
-        bonds.bond{value: 1 ether}(user);
+        uint256 tokenIdTwo = bonds.bond{value: 1 ether}(user);
 
-        // strike price should be different between the two (goes up)
-        assertLt(
-            stratOption.strikeAmount(1),
-            stratOption.strikeAmount(2),
-            "ETH price increase should increase total strike amount"
-        );
+        IStratOptionMinter.Option memory optionOne = stratOption.getOption(tokenIdOne);
+        IStratOptionMinter.Option memory optionTwo = stratOption.getOption(tokenIdTwo);
+
+        // strike price should increase with the increase in ETH price
+        assertLt(optionOne.strikePrice, optionTwo.strikePrice, "ETH price increase should increase strike price");
 
         // Changes in STRAT/ETH price don't change the notional USD (but the calculated strike price should move about)
         uint256 strikeBeforePriceChange = bonds.strikePrice(0);
@@ -337,42 +360,51 @@ contract StratETHLongBondsTest is Test {
             "STRAT price increase should increase the bond strike per strat"
         );
 
+        // Bond 1 more ETH, after STRAT/ETH price goes up to 1.5 ETH/STRAT
         vm.deal(user, 1 ether);
         vm.prank(user);
-        bonds.bond{value: 1 ether}(user);
+        uint256 tokenIdThree = bonds.bond{value: 1 ether}(user);
+        IStratOptionMinter.Option memory optionThree = stratOption.getOption(tokenIdThree);
+        uint256 balanceTwo = stratOption.balanceOf(user, tokenIdTwo);
+        uint256 balanceThree = stratOption.balanceOf(user, tokenIdThree);
+
+        // Redemption amount should be the same
         assertEq(
-            stratOption.notionalUSDAmount(2),
-            stratOption.notionalUSDAmount(3),
-            "STRAT price increase shouldn't effect notional USD"
+            optionTwo.redemptionPrice * balanceTwo / 1e18,
+            optionThree.redemptionPrice * balanceThree / 1e18,
+            "STRAT price increase shouldn't effect redemption amount"
         );
 
         strikeBeforePriceChange = bonds.strikePrice(0);
         stratEthOracle.setPrice(0.5e18);
         strikeAfterPriceChange = bonds.strikePrice(0);
-        assertGt(
-            strikeBeforePriceChange,
+        assertLt(
             strikeAfterPriceChange,
+            strikeBeforePriceChange,
             "STRAT price decrease should decrease the bond strike per strat"
         );
 
         vm.deal(user, 1 ether);
         vm.prank(user);
-        bonds.bond{value: 1 ether}(user);
+        uint256 tokenIdFour = bonds.bond{value: 1 ether}(user);
+        IStratOptionMinter.Option memory optionFour = stratOption.getOption(tokenIdFour);
+        uint256 balanceFour = stratOption.balanceOf(user, tokenIdFour);
+
+        // Redemption amount should be the same
         assertEq(
-            stratOption.notionalUSDAmount(3),
-            stratOption.notionalUSDAmount(4),
-            "STRAT price decrease shouldn't effect notional USD"
+            optionThree.redemptionPrice * balanceThree / 1e18,
+            optionFour.redemptionPrice * balanceFour / 1e18,
+            "STRAT price decrease shouldn't effect redemption amount"
         );
 
         // Bond 1 more ETH, after ETH/USD price goes down to $3000
         ethUsdOracle.setPrice(3000e8);
         vm.deal(user, 1 ether);
         vm.prank(user);
-        bonds.bond{value: 1 ether}(user);
-        assertGt(
-            stratOption.strikeAmount(4),
-            stratOption.strikeAmount(5),
-            "ETH price decrease should decrease total strike amount"
-        );
+        uint256 tokenIdFive = bonds.bond{value: 1 ether}(user);
+        IStratOptionMinter.Option memory optionFive = stratOption.getOption(tokenIdFive);
+        uint256 balanceFive = stratOption.balanceOf(user, tokenIdFive);
+
+        assertGt(balanceFour, balanceFive, "ETH price decrease should decrease total STRAT output");
     }
 }
