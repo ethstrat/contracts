d89ac5a3f77d85932d58b4463d93768035703755
Napenjoyoor <napenjoyoor@proton.me>
2025-04-04 00:58:22 +1100

    chore: removed console.log import

--- src/MorphoMarket/StratMorphoMarket.sol
+++ src/MorphoMarket/StratMorphoMarket.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.20;
 
-import "forge-std/console.sol";
-
 import {IERC20, IERC20MintableBurnable} from "../interfaces/IERC20.sol";
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
