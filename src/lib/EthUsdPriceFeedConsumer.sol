2e1674b71c7ff40d951693f87e00daf2c69eac4a
Jem <0x0xjem@gmail.com>
2025-04-07 20:08:06 +0400

    feat: validate ETH-USD oracle upon deployment, avoid conflicts with token constants

--- src/lib/EthUsdPriceFeedConsumer.sol
+++ src/lib/EthUsdPriceFeedConsumer.sol
@@ -10,21 +10,27 @@ import {IPriceOracle} from "../interfaces/IPriceOracle.sol";
 abstract contract EthUsdPriceFeedConsumer {
     IPriceOracle public immutable ethUsdOracle;
 
-    address internal constant _WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant _ETH_USD_WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     /**
      * @dev Euler USD special designator. Has a scale of 18.
      *      See:
      * https://github.com/euler-xyz/euler-price-oracle/blob/15c6e68bd62e108a8243d9a61c843c5981d75477/src/adapter/BaseAdapter.sol#L38
      */
-    address internal constant _USD = 0x0000000000000000000000000000000000000348;
+    address internal constant _ETH_USD_USD = 0x0000000000000000000000000000000000000348;
 
+    /**
+     * @dev Can be pre-set as both wETH and USD have a scale of 18.
+     */
     uint256 internal constant _ETH_USD_ORACLE_SCALE = 1e18;
 
     error EthUsdPriceInvalid();
 
     constructor(address _ethUsdOracle) {
         ethUsdOracle = IPriceOracle(_ethUsdOracle);
+
+        // Validate that the oracle is correct
+        _getEthUsdPrice();
     }
 
     /**
@@ -36,7 +42,7 @@ abstract contract EthUsdPriceFeedConsumer {
         // Quantity of USD for 1 WETH
         // This will revert if the base or quote tokens are not as expected,
         // which would indicate that the incorrect IPriceOracle was provided.
-        price = ethUsdOracle.getQuote(1e18, _WETH, _USD);
+        price = ethUsdOracle.getQuote(1e18, _ETH_USD_WETH, _ETH_USD_USD);
 
         // Revert if the price is not returned
         if (price == 0) revert EthUsdPriceInvalid();
