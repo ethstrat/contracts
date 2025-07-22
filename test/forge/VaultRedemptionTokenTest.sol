5883afc525586afbf3309104636529f4ba208838
Jem <0x0xjem@gmail.com>
2025-07-23 01:08:15 +1000

    test: check values of totalClaimableShares/accClaimPerShare/maxRedeemableBy

--- test/forge/VaultRedemptionTokenTest.sol
+++ test/forge/VaultRedemptionTokenTest.sol
@@ -36,73 +36,233 @@ contract VaultRedemptionTokenTest is Test {
     function testSingleUserFullRedemption() public {
         mintClaimableShares(user1, 100 ether);
 
-        // nothing should be nothing to redeem if there are no assets
-        // to redeem
+        assertEq(vaultRedemptionToken.totalClaimableShares(), 100 ether, "post-mint totalClaimableShares"); // Matches
+            // claimable shares
+        assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 0, "post-mint redeemOffsetOf"); // Nothing redeemed
+        assertEq(vaultRedemptionToken.accClaimPerShare(), 0, "post-mint accClaimPerShare"); // No claimable amount
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0, "post-mint maxRedeemableBy"); // 100% of claimable
+            // amount (0 ether)
+
+        // nothing to redeem if there are no assets
         uint256 redeemed = vaultRedemptionToken.redeem(user1, user1);
-        assertEq(redeemed, 0);
-        assertEq(vaultRedemptionToken.balanceOf(user1), 100 ether);
+        assertEq(redeemed, 0, "redeemed");
+        assertEq(vaultRedemptionToken.balanceOf(user1), 100 ether, "post-redeem balanceOf");
 
         // If there is a single asset to redeem, and a single user, they
         // should be able to redeem all of the asset
         vaultRedemptionToken.increaseClaimableAmount(1 ether);
+
+        assertEq(
+            vaultRedemptionToken.totalClaimableShares(), 100 ether, "post-increaseClaimableAmount totalClaimableShares"
+        ); // Matches claimable shares
+        assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 0, "post-increaseClaimableAmount redeemOffsetOf"); // Nothing
+            // redeemed
+        assertEq(
+            vaultRedemptionToken.accClaimPerShare(),
+            1e18 * 1e18 / 100e18,
+            "post-increaseClaimableAmount accClaimPerShare"
+        ); // 1 ether / 100 ether = 0.01
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 1 ether, "post-increaseClaimableAmount maxRedeemableBy"); // 100%
+            // of claimable amount (1 ether)
+
         vm.prank(user1);
         redeemed = vaultRedemptionToken.redeem(user1, user1);
-        assertEq(redeemed, 1 ether);
-        assertEq(vaultRedemptionToken.balanceOf(user1), 99 ether);
-        assertEq(asset.balanceOf(user1), 1 ether);
-        assertEq(asset.balanceOf(address(vaultRedemptionToken)), 0);
+        assertEq(redeemed, 1 ether, "redeemed2");
+        assertEq(vaultRedemptionToken.balanceOf(user1), 99 ether, "post-redeem2 balanceOf");
+        assertEq(asset.balanceOf(user1), 1 ether, "post-redeem2 asset.balanceOf(user1)");
+        assertEq(
+            asset.balanceOf(address(vaultRedemptionToken)), 0, "post-redeem2 asset.balanceOf(vaultRedemptionToken)"
+        );
+
+        assertEq(vaultRedemptionToken.totalClaimableShares(), 99 ether, "post-redeem2 totalClaimableShares"); // 100
+            // ether - 1 ether redeemed
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 1 ether, "post-redeem2 redeemOffsetOf"); // 1 ether
+        // redeemed
+        assertEq(vaultRedemptionToken.accClaimPerShare(), 1e18 * 1e18 / 100e18, "post-redeem2 accClaimPerShare"); // 1
+            // ether / 100 ether = 0.01
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0, "post-redeem2 maxRedeemableBy"); // 100% of remaining
+            // claimable amount (0 ether)
 
         // If we increase the claimable amount to the remaining 99
         // shares, the user should be able to redeem their entire balance
         vaultRedemptionToken.increaseClaimableAmount(99 ether);
+
+        assertEq(
+            vaultRedemptionToken.totalClaimableShares(), 99 ether, "post-increaseClaimableAmount2 totalClaimableShares"
+        ); // 100 ether - 1 ether redeemed
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 1 ether, "post-increaseClaimableAmount2
+        // redeemOffsetOf"); // 1 ether redeemed
+        assertEq(
+            vaultRedemptionToken.accClaimPerShare(),
+            1e18 * 1e18 / 100e18 + 99e18 * 1e18 / 99e18,
+            "post-increaseClaimableAmount2 accClaimPerShare"
+        ); // 1.01e18 = 101e16
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 99e18, "post-increaseClaimableAmount2 maxRedeemableBy"); // 100%
+            // of remaining claimable amount (99 ether)
+
         vm.prank(user1);
         redeemed = vaultRedemptionToken.redeem(user1, user1);
-        assertEq(redeemed, 99 ether);
-        assertEq(vaultRedemptionToken.balanceOf(user1), 0);
-        assertEq(asset.balanceOf(user1), 100 ether);
-        assertEq(asset.balanceOf(address(vaultRedemptionToken)), 0);
+        assertEq(redeemed, 99 ether, "redeemed3");
+        assertEq(vaultRedemptionToken.balanceOf(user1), 0, "post-redeem3 balanceOf");
+        assertEq(asset.balanceOf(user1), 100 ether, "post-redeem3 asset.balanceOf(user1)");
+        assertEq(
+            asset.balanceOf(address(vaultRedemptionToken)), 0, "post-redeem3 asset.balanceOf(vaultRedemptionToken)"
+        );
+
+        assertEq(vaultRedemptionToken.totalClaimableShares(), 0, "post-redeem3 totalClaimableShares"); // 100 ether - 1
+            // ether - 99 ether = 0
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 100 ether, "post-redeem3 redeemOffsetOf"); // 100 ether
+        // redeemed
+        assertEq(
+            vaultRedemptionToken.accClaimPerShare(),
+            1e18 * 1e18 / 100e18 + 99e18 * 1e18 / 99e18,
+            "post-redeem3 accClaimPerShare"
+        ); // 1.01e18 = 101e16
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0, "post-redeem3 maxRedeemableBy"); // 100% of remaining
+            // claimable amount (0 ether)
     }
 
     function testTwoUsersProportionalRedemption() public {
         mintClaimableShares(user1, 100 ether);
         mintClaimableShares(user2, 100 ether);
+
+        assertEq(vaultRedemptionToken.totalClaimableShares(), 200 ether, "post-mint totalClaimableShares"); // Matches
+            // claimable shares
+        assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 0, "post-mint redeemOffsetOf.user1"); // Nothing redeemed
+        assertEq(vaultRedemptionToken.redeemOffsetOf(user2), 0, "post-mint redeemOffsetOf.user2"); // Nothing redeemed
+        assertEq(vaultRedemptionToken.accClaimPerShare(), 0, "post-mint accClaimPerShare"); // No claimable amount
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0, "post-mint maxRedeemableBy.user1"); // 100% of
+            // claimable amount (0 ether)
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user2), 0, "post-mint maxRedeemableBy.user2"); // 100% of
+            // claimable amount (0 ether)
+
         vaultRedemptionToken.increaseClaimableAmount(2 ether);
+
+        assertEq(
+            vaultRedemptionToken.totalClaimableShares(), 200 ether, "post-increaseClaimableAmount totalClaimableShares"
+        ); // Matches claimable shares
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 0, "post-increaseClaimableAmount redeemOffsetOf.user1");
+        // // Nothing redeemed
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user2), 0, "post-increaseClaimableAmount redeemOffsetOf.user2");
+        // // Nothing redeemed
+        assertEq(
+            vaultRedemptionToken.accClaimPerShare(),
+            2e18 * 1e18 / 200e18,
+            "post-increaseClaimableAmount accClaimPerShare"
+        ); // 2 ether / 200 ether = 0.01
+
+        assertEq(
+            vaultRedemptionToken.maxRedeemableBy(user1), 1 ether, "post-increaseClaimableAmount maxRedeemableBy.user1"
+        ); // 50% of claimable amount (2 ether)
+        assertEq(
+            vaultRedemptionToken.maxRedeemableBy(user2), 1 ether, "post-increaseClaimableAmount maxRedeemableBy.user2"
+        ); // 50% of claimable amount (2 ether)
+
         uint256 redeemed = 0;
 
         // Both users should be able to redeem 1 asset each
         vm.prank(user1);
         redeemed = vaultRedemptionToken.redeem(user1, user1);
-        assertEq(redeemed, 1 ether);
+        assertEq(redeemed, 1 ether, "redeemed.user1");
 
         vm.prank(user2);
         redeemed = vaultRedemptionToken.redeem(user2, user2);
-        assertEq(redeemed, 1 ether);
+        assertEq(redeemed, 1 ether, "redeemed.user2");
 
-        assertEq(vaultRedemptionToken.balanceOf(user1), 99 ether);
-        assertEq(vaultRedemptionToken.balanceOf(user2), 99 ether);
-        assertEq(asset.balanceOf(user1), 1 ether);
-        assertEq(asset.balanceOf(user2), 1 ether);
-        assertEq(asset.balanceOf(address(vaultRedemptionToken)), 0);
+        assertEq(vaultRedemptionToken.balanceOf(user1), 99 ether, "post-redeem balanceOf.user1");
+        assertEq(vaultRedemptionToken.balanceOf(user2), 99 ether, "post-redeem balanceOf.user2");
+        assertEq(asset.balanceOf(user1), 1 ether, "post-redeem asset.balanceOf.user1");
+        assertEq(asset.balanceOf(user2), 1 ether, "post-redeem asset.balanceOf.user2");
+        assertEq(asset.balanceOf(address(vaultRedemptionToken)), 0, "post-redeem asset.balanceOf(vaultRedemptionToken)");
+
+        assertEq(vaultRedemptionToken.totalClaimableShares(), 198 ether, "post-redeem totalClaimableShares"); // 200
+            // ether - 2 ether redeemed
+        uint256 previousTotalClaimableShares = vaultRedemptionToken.totalClaimableShares();
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 1 ether, "post-redeem redeemOffsetOf.user1"); // 1 ether
+        // redeemed
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user2), 1 ether, "post-redeem redeemOffsetOf.user2"); // 1 ether
+        // redeemed
+        assertEq(vaultRedemptionToken.accClaimPerShare(), 2e18 * 1e18 / 200e18, "post-redeem accClaimPerShare"); // 2
+            // ether / 200 ether = 0.01
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0, "post-redeem maxRedeemableBy.user1"); // 50% of
+            // claimable amount (0 ether)
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user2), 0, "post-redeem maxRedeemableBy.user2"); // 50% of
+            // claimable amount (0 ether)
 
         // Remains proportional after increasing claimable amount
         // if only one user redeems
         vaultRedemptionToken.increaseClaimableAmount(2 ether);
         redeemed = vaultRedemptionToken.redeem(user1, user1);
-        assertApproxEqRel(redeemed, 1 ether, ONE_PERCENT);
+        assertApproxEqAbs(redeemed, 1 ether, 1, "post-redeem2 redeemed");
+
+        assertEq(
+            vaultRedemptionToken.totalClaimableShares(),
+            previousTotalClaimableShares - redeemed,
+            "post-redeem2 totalClaimableShares"
+        ); // 198e18 - 1e18 = 197e18
+        previousTotalClaimableShares = vaultRedemptionToken.totalClaimableShares();
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user1), 1 ether + redeemed, "post-redeem2
+        // redeemOffsetOf.user1"); // 1 ether redeemed + redeemed
+        // assertEq(vaultRedemptionToken.redeemOffsetOf(user2), 1 ether, "post-redeem2 redeemOffsetOf.user2"); // 1
+        // ether redeemed
+        assertEq(vaultRedemptionToken.accClaimPerShare(), 20101010101010101, "post-redeem2 accClaimPerShare"); // 2e18 *
+            // 1e18 / 200e18 + 2e18 * 1e18 / 198e18 = 10000000000000000 + 10101010101010101 = 20101010101010101
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0 ether, "post-redeem2 maxRedeemableBy.user1"); // Already
+            // claimed
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user2), redeemed, "post-redeem2 maxRedeemableBy.user2"); // 50% of
+            // claimable amount (2 ether)
 
         vaultRedemptionToken.increaseClaimableAmount(2 ether);
         vm.prank(user1);
         redeemed = vaultRedemptionToken.redeem(user1, user1);
-        assertApproxEqRel(redeemed, 1 ether, ONE_PERCENT);
+        assertApproxEqAbs(redeemed, 1 ether, 1, "post-redeem3 redeemed");
+
+        assertEq(
+            vaultRedemptionToken.totalClaimableShares(),
+            previousTotalClaimableShares - redeemed,
+            "post-redeem3 totalClaimableShares"
+        ); // 197e18 - 1e18 = 196e18
+        previousTotalClaimableShares = vaultRedemptionToken.totalClaimableShares();
+        assertEq(vaultRedemptionToken.accClaimPerShare(), 30253294364969491, "post-redeem3 accClaimPerShare"); // 2e18 *
+            // 1e18 / 200e18 + 2e18 * 1e18 / 198e18 + 2e18 * 1e18 / 197e18 = 10000000000000000 + 10101010101010101 +
+            // 10152284263959390 = 30253294364969491
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0 ether, "post-redeem3 maxRedeemableBy.user1"); // Already
+            // claimed
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user2), 2 ether, "post-redeem3 maxRedeemableBy.user2"); // 50% of
+            // claimable amount (4 ether)
 
         vm.prank(user2);
         redeemed = vaultRedemptionToken.redeem(user2, user2);
-        assertApproxEqRel(redeemed, 2 ether, ONE_PERCENT);
+        assertApproxEqAbs(redeemed, 2 ether, 1, "post-redeem4 redeemed");
 
         assertApproxEqRel(vaultRedemptionToken.balanceOf(user1), 97 ether, ONE_PERCENT);
         assertApproxEqRel(vaultRedemptionToken.balanceOf(user2), 97 ether, ONE_PERCENT);
         assertApproxEqRel(asset.balanceOf(user1), 3 ether, ONE_PERCENT);
         assertApproxEqRel(asset.balanceOf(user2), 3 ether, ONE_PERCENT);
+
+        assertEq(
+            vaultRedemptionToken.totalClaimableShares(),
+            previousTotalClaimableShares - redeemed,
+            "post-redeem4 totalClaimableShares"
+        ); // 196e18 - 2e18 = 194e18
+        previousTotalClaimableShares = vaultRedemptionToken.totalClaimableShares();
+        assertEq(vaultRedemptionToken.accClaimPerShare(), 30253294364969491, "post-redeem3 accClaimPerShare"); // 2e18 *
+            // 1e18 / 200e18 + 2e18 * 1e18 / 198e18 + 2e18 * 1e18 / 197e18 = 10000000000000000 + 10101010101010101 +
+            // 10152284263959390 = 30253294364969491
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0 ether, "post-redeem3 maxRedeemableBy.user1"); // Already
+            // claimed
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user2), 0 ether, "post-redeem3 maxRedeemableBy.user2"); // Already
+            // claimed
     }
 
     function testProportionalAfterPartialRedemptionAndNewJoiner() public {
@@ -113,11 +273,37 @@ contract VaultRedemptionTokenTest is Test {
         vm.prank(user1);
         vaultRedemptionToken.redeem(user1, user1);
 
+        assertEq(vaultRedemptionToken.totalClaimableShares(), 50 ether, "post-redeem totalClaimableShares"); // 100
+            // ether - 50 ether redeemed
+        assertEq(vaultRedemptionToken.accClaimPerShare(), 50e18 * 1e18 / 100e18, "post-redeem accClaimPerShare"); // 50
+            // ether / 100 ether = 0.01
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0 ether, "post-redeem maxRedeemableBy.user1"); // Already
+            // claimed
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user2), 0 ether, "post-redeem maxRedeemableBy.user2"); // No
+            // shares
+
         // user2 joins with 50 shares
         mintClaimableShares(user2, 50 ether);
 
         // Add an additional 50 units to redeem
         vaultRedemptionToken.increaseClaimableAmount(50 ether);
+
+        assertEq(
+            vaultRedemptionToken.totalClaimableShares(), 100 ether, "post-increaseClaimableAmount totalClaimableShares"
+        ); // 100 ether - 50 ether redeemed + 50 ether minted
+        assertEq(
+            vaultRedemptionToken.accClaimPerShare(),
+            50e18 * 1e18 / 100e18 + 50e18 * 1e18 / 100e18,
+            "post-increaseClaimableAmount accClaimPerShare"
+        );
+
+        assertEq(
+            vaultRedemptionToken.maxRedeemableBy(user1), 25 ether, "post-increaseClaimableAmount maxRedeemableBy.user1"
+        ); // 50/100 * 50 = 25
+        assertEq(
+            vaultRedemptionToken.maxRedeemableBy(user2), 25 ether, "post-increaseClaimableAmount maxRedeemableBy.user2"
+        ); // 50/100 * 50 = 25
 
         // Both redeem max
         vm.prank(user1);
@@ -131,6 +317,19 @@ contract VaultRedemptionTokenTest is Test {
         assertApproxEqRel(redeem2, 25 ether, ONE_PERCENT);
         assertApproxEqRel(asset.balanceOf(user1), 75 ether, ONE_PERCENT);
         assertApproxEqRel(asset.balanceOf(user2), 25 ether, ONE_PERCENT);
+
+        assertEq(vaultRedemptionToken.totalClaimableShares(), 50 ether, "post-redeem2 totalClaimableShares"); // 100
+            // ether - 50 ether redeemed + 50 ether minted - 25 ether redeemed - 25 ether redeemed = 50 ether
+        assertEq(
+            vaultRedemptionToken.accClaimPerShare(),
+            50e18 * 1e18 / 100e18 + 50e18 * 1e18 / 100e18,
+            "post-redeem2 accClaimPerShare"
+        );
+
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user1), 0 ether, "post-redeem2 maxRedeemableBy.user1"); // Already
+            // claimed
+        assertEq(vaultRedemptionToken.maxRedeemableBy(user2), 0 ether, "post-redeem2 maxRedeemableBy.user2"); // Already
+            // claimed
     }
 
     function testClaimedTransferredOnTransfer() public {
@@ -162,4 +361,11 @@ contract VaultRedemptionTokenTest is Test {
         vm.expectRevert(VaultRedemptionToken.ClaimableExceedsBalance.selector);
         vaultRedemptionToken.increaseClaimableSharesFor(user1, 50 ether);
     }
+
+    // [ ] redeemOffsetOf when redeeming
+    // [ ] redeemOffsetOf when burning
+    // [ ] redeemOffsetOf when transferring
+    // [ ] access controls for redeem
+    // [ ] access controls for increaseClaimableSharesFor
+    // [ ] access controls for increaseClaimableAmount
 }
