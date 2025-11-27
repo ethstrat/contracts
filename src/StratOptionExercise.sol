6ebb391f8747621784d6613cc219ff335fced4b1
Napenjoyoor <napenjoyoor@users.noreply.github.com>
2025-11-27 17:07:44 +1100

    chore: updated solc version + added deps

--- src/StratOptionExercise.sol
+++ src/StratOptionExercise.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import {IERC20MintableBurnable, IERC20MintableBurnablePermit} from "./interfaces/IERC20.sol";
 import {IStratOption} from "./interfaces/IStratOption.sol";
